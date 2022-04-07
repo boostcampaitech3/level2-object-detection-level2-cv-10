fb2fa5472fa3cb75fa56253f0d6f3047a0645fe9
seungriyou <seungri0826@gmail.com>
2022-04-07 14:05:50 +0000

    [EXP] : Upload personal folder - configs/_seungri_
    날짜별로 생성했던 실험용 디렉토리와 파일들도 업로드합니다.
    close #21

---
+++ mmdetection/configs/_seungri_/0402-bbox_head/0402_cascade_rcnn_swin_l_fpn_bbox_head_fold0.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/cascade_rcnn_swin_l_fpn_bbox_head.py',
+    '../_base_/datasets/coco_detection_albu.py',
+    '../_base_/schedules/schedule_cosann_adamw.py', '../_base_/default_runtime.py'
+]
---
+++ mmdetection/configs/_seungri_/0404-modifications/0404_universenet_swin_l_gfl.py
@@ -0,0 +1,17 @@
+_base_ = [
+    '../_base_/models/universenet_swin_l_gfl.py',
+    '../_base_/datasets/coco_detection_albu.py',
+    '../_base_/schedules/schedule_universenet.py', '../_base_/default_runtime.py'
+]
+
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+        # mlflow
+        dict(
+            type='MlflowLoggerHook',
+            exp_name='0404_universenet',
+        )
+    ])
